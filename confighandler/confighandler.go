95422e310f13fd645782ba3dd0774097f486ee72
Schröter, Daniel Christian (BnotK) <d.schroeter@bnotk.de>
2023-01-30 09:27:48 +0100

    Rename MB_PRIVATEBIN_HOST to MB_PRIVATEBIN_URL and  MB_GOVC_HOST to MB_GOVC_URL

--- confighandler/confighandler.go
+++ confighandler/confighandler.go
@@ -34,7 +34,7 @@ type Config struct {
 	MB_MATTERMOST_TEAM              string
 	MB_MATTERMOST_CHANNEL           string
 	MB_PRIVATEBIN_ENABLE            string
-	MB_PRIVATEBIN_HOST              string
+	MB_PRIVATEBIN_URL               string
 	MB_PRIVATEBIN_FORMATTER         string
 	MB_PRIVATEBIN_EXPIRE            string
 	MB_PRIVATEBIN_OPENDISCUSSION    string
@@ -44,23 +44,13 @@ type Config struct {
 	MB_TERRAFORM_WRAPPER_EXECUTABLE string
 	MB_OC_EXECUTABLE                string
 	MB_GOVC_EXECUTABLE              string
-	MB_GOVC_HOST                    string
+	MB_GOVC_URL                     string
 	MB_GOVC_DATACENTER              string
 	MB_GOVC_USERNAME                string
 	MB_GOVC_PASSWORD                string
 }
 
 func Init() {
-	// App = &Application{
-	// 	Logger: zerolog.New(
-	// 		zerolog.ConsoleWriter{
-	// 			Out:        os.Stdout,
-	// 			TimeFormat: time.RFC822,
-	// 			NoColor:    true,
-	// 		},
-	// 	).With().Timestamp().Logger(),
-	// }
-
 	App = &Application{
 		Logger: zerolog.New(
 			os.Stdout,
@@ -75,8 +65,7 @@ func LoadConfig() Config {
 
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("env")    // REQUIRED if the config file does not have the extension in the name
-	//viper.AddConfigPath("/etc/bitbucket-mattermost-notifier/") // path to look for the config file in
-	viper.AddConfigPath(".") // optionally look for config in the working directory
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
 	viper.AutomaticEnv()
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
@@ -91,14 +80,6 @@ func LoadConfig() Config {
 	if GetConfigBoolValue("MB_DEBUG") {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
-	// if !checkParams("MATTERMOST_WEBHOOKURL", "MATTERMOST_CHANNEL") {
-	// 	log.Fatal("MATTERMOST_WEBHOOKURL or MATTERMOST_CHANNEL can NOT be empty")
-	// }
-	// settings.mattermostTeamName = os.Getenv("MM_TEAM")
-	// settings.mattermostUserName = os.Getenv("MM_USERNAME")
-	// settings.mattermostToken = os.Getenv("MM_TOKEN")
-	// settings.mattermostChannel = os.Getenv("MM_CHANNEL")
-	// settings.mattermostServer, _ = url.Parse(os.Getenv("MM_SERVER"))
 
 	settings := map[string]string{
 		"MB_ROLES":                        GetConfigValue("MB_ROLES"),
@@ -107,7 +88,7 @@ func LoadConfig() Config {
 		"MB_MATTERMOST_TEAM":              GetConfigValue("MB_MATTERMOST_TEAM"),
 		"MB_MATTERMOST_CHANNEL":           GetConfigValue("MB_MATTERMOST_CHANNEL"),
 		"MB_PRIVATEBIN_ENABLE":            GetConfigValue("MB_PRIVATEBIN_ENABLE"),
-		"MB_PRIVATEBIN_HOST":              GetConfigValue("MB_PRIVATEBIN_HOST"),
+		"MB_PRIVATEBIN_URL":               GetConfigValue("MB_PRIVATEBIN_URL"),
 		"MB_PRIVATEBIN_FORMATTER":         GetConfigValue("MB_PRIVATEBIN_FORMATTER"),
 		"MB_PRIVATEBIN_EXPIRE":            GetConfigValue("MB_PRIVATEBIN_EXPIRE"),
 		"MB_PRIVATEBIN_OPENDISCUSSION":    GetConfigValue("MB_PRIVATEBIN_OPENDISCUSSION"),
@@ -117,35 +98,11 @@ func LoadConfig() Config {
 		"MB_TERRAFORM_WRAPPER_EXECUTABLE": GetConfigValue("MB_TERRAFORM_WRAPPER_EXECUTABLE"),
 		"MB_OC_EXECUTABLE":                GetConfigValue("MB_OC_EXECUTABLE"),
 		"MB_GOVC_EXECUTABLE":              GetConfigValue("MB_GOVC_EXECUTABLE"),
-		"MB_GOVC_HOST":                    GetConfigValue("MB_GOVC_HOST"),
+		"MB_GOVC_URL":                     GetConfigValue("MB_GOVC_URL"),
 		"MB_GOVC_DATACENTER":              GetConfigValue("MB_GOVC_DATACENTER"),
 		"MB_GOVC_USERNAME":                GetConfigValue("MB_GOVC_USERNAME"),
 		"MB_GOVC_PASSWORD":                GetConfigValue("MB_GOVC_PASSWORD"),
 	}
-
-	// Settings.MB_ROLES = GetConfigValue("MB_ROLES")
-	// Settings.MB_MATTERMOST_URL = GetConfigValue("MB_MATTERMOST_URL")
-	// Settings.MB_MATTERMOST_USERTOKEN = GetConfigValue("MB_MATTERMOST_USERTOKEN")
-	// Settings.MB_MATTERMOST_TEAM = GetConfigValue("MB_MATTERMOST_TEAM")
-	// Settings.MB_MATTERMOST_CHANNEL = GetConfigValue("MB_MATTERMOST_CHANNEL")
-	// Settings.MB_PRIVATEBIN_ENABLE = GetConfigBoolValue("MB_PRIVATEBIN_ENABLE")
-	// Settings.MB_PRIVATEBIN_HOST = GetConfigValue("MB_PRIVATEBIN_HOST")
-	// Settings.MB_PRIVATEBIN_FORMATTER = GetConfigValue("MB_PRIVATEBIN_FORMATTER")
-	// Settings.MB_PRIVATEBIN_EXPIRE = GetConfigValue("MB_PRIVATEBIN_EXPIRE")
-	// Settings.MB_PRIVATEBIN_OPENDISCUSSION = GetConfigBoolValue("MB_PRIVATEBIN_OPENDISCUSSION")
-	// Settings.MB_PRIVATEBIN_BURNAFTERREADING = GetConfigBoolValue("MB_PRIVATEBIN_BURNAFTERREADING")
-	// Settings.MB_PRIVATEBIN_PASSWORD = GetConfigValue("MB_PRIVATEBIN_PASSWORD")
-	// Settings.MB_TERRAFORM_EXECUTABLE = GetConfigValue("MB_TERRAFORM_EXECUTABLE")
-	// Settings.MB_TERRAFORM_WRAPPER_EXECUTABLE = GetConfigValue("MB_TERRAFORM_WRAPPER_EXECUTABLE")
-	// Settings.MB_OC_EXECUTABLE = GetConfigValue("MB_OC_EXECUTABLE")
-	// Settings.MB_GOVC_EXECUTABLE = GetConfigValue("MB_GOVC_EXECUTABLE")
-	// Settings.MB_GOVC_HOST = GetConfigValue("MB_GOVC_HOST")
-	// Settings.MB_GOVC_DATACENTER = GetConfigValue("MB_GOVC_DATACENTER")
-	// Settings.MB_GOVC_USERNAME = GetConfigValue("MB_GOVC_USERNAME")
-	// Settings.MB_GOVC_PASSWORD = GetConfigValue("MB_GOVC_PASSWORD")
-
-	// TODO: No secret credentials in output
-	//App.Logger.Info().Msg(fmt.Sprint(Settings))
 
 	// Output all keys and values of the current loaded config
 	message := "Current configuration: "
@@ -166,7 +123,7 @@ func LoadConfig() Config {
 		MB_MATTERMOST_TEAM:              settings["MB_MATTERMOST_TEAM"],
 		MB_MATTERMOST_CHANNEL:           settings["MB_MATTERMOST_CHANNEL"],
 		MB_PRIVATEBIN_ENABLE:            settings["MB_PRIVATEBIN_ENABLE"],
-		MB_PRIVATEBIN_HOST:              settings["MB_PRIVATEBIN_HOST"],
+		MB_PRIVATEBIN_URL:               settings["MB_PRIVATEBIN_URL"],
 		MB_PRIVATEBIN_FORMATTER:         settings["MB_PRIVATEBIN_FORMATTER"],
 		MB_PRIVATEBIN_EXPIRE:            settings["MB_PRIVATEBIN_EXPIRE"],
 		MB_PRIVATEBIN_OPENDISCUSSION:    settings["MB_PRIVATEBIN_OPENDISCUSSION"],
@@ -176,7 +133,7 @@ func LoadConfig() Config {
 		MB_TERRAFORM_WRAPPER_EXECUTABLE: settings["MB_TERRAFORM_WRAPPER_EXECUTABLE"],
 		MB_OC_EXECUTABLE:                settings["MB_OC_EXECUTABLE"],
 		MB_GOVC_EXECUTABLE:              settings["MB_GOVC_EXECUTABLE"],
-		MB_GOVC_HOST:                    settings["MB_GOVC_HOST"],
+		MB_GOVC_URL:                     settings["MB_GOVC_URL"],
 		MB_GOVC_DATACENTER:              settings["MB_GOVC_DATACENTER"],
 		MB_GOVC_USERNAME:                settings["MB_GOVC_USERNAME"],
 		MB_GOVC_PASSWORD:                settings["MB_GOVC_PASSWORD"],
