14a273157ff9eecfaf928549cc150106dfd81658
Daniel Christian Schröter <2836188+danielchristianschroeter@users.noreply.github.com>
2023-02-03 09:08:13 +0100

    rework Terraform wrapper script

--- misc/tf.sh
+++ misc/tf.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 # Terraform wrapper script
 set -e -o pipefail
-ACTION=$1
-ENV=$2
-ZONE=$3
+
+ACTION="$1"
+ENV="$2"
+ZONE="$3"
+TF_DIR="/opt/terraform/environments/$ENV/$ZONE"
+LOG_DIR="/opt/terraform/log/"
+DATE=$(date +'%Y%m%d-%H:%M:%S')
+LOG_FILE="$LOG_DIR/${DATE}_terraform_apply.log"
 
 if [ -z "$ACTION" ] || [ -z "$ENV" ] || [ -z "$ZONE" ]; then
     echo "Usage: tf <plan or apply> <env> <zone>"
     exit 1
 fi
 
-if [[ ! -d "/opt/terraform/environments/$ENV/$ZONE" ]]; then
-    echo "/opt/terraform/environments/$ENV/$ZONE not found."
+if [[ ! -d "$TF_DIR" ]]; then
+    echo "$TF_DIR not found."
     exit 1
 fi
 
 if [[ "$ACTION" == "plan" ]]; then
-  terraform -chdir=/opt/terraform/environments/$ENV/$ZONE plan -no-color >/dev/null -input=false -out=tfplan
-  terraform -chdir=/opt/terraform/environments/$ENV/$ZONE show tfplan -no-color
-  echo "To apply this plan, run: !tf apply $ENV $ZONE"
+  terraform -chdir="$TF_DIR" plan -no-color >/dev/null -input=false -out=tfplan \
+    && terraform -chdir="$TF_DIR" show tfplan -no-color \
+    && echo "To apply this plan, run: !tf apply $ENV $ZONE"
 fi
 
 if [[ "$ACTION" == "apply" ]]; then
-  terraform -chdir=/opt/terraform/environments/$ENV/$ZONE apply -no-color -input=false -auto-approve tfplan
+  if [ -d "$LOG_DIR" ]; then
+    if [ -f "$TF_DIR/tfplan" ]; then
+      terraform -chdir="$TF_DIR" show -no-color tfplan | tee -a "$LOG_FILE" \
+        && terraform -chdir="$TF_DIR" apply -no-color -input=false -auto-approve tfplan | tee -a "$LOG_FILE"
+    else
+      echo "Error: tfplan not found in $TF_DIR"
+      exit 1
+    fi
+  else
+    echo "Error: log directory $LOG_DIR not found"
+    exit 1
+  fi
 fi
-
